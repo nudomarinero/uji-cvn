c4fbe3de99fde29ff01e2df56f24550571ec4245
Maria Sirvent <msirvent@uji.es>
2017-02-23 09:25:06 +0000

    Añadida la creación de tablas con datos de producción de PCI para tener los datos en desarrollo.
    git-svn-id: http://jira.uji.es/repos/uji/CVN/uji-cvn/trunk@33772 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-cvn-base/src/main/resources/database/uji_cvn_pci_tablas_dev.sql
+++ uji-cvn-base/src/main/resources/database/uji_cvn_pci_tablas_dev.sql
@@ -1,3 +1,59 @@
+drop table pci_revistas_cuartiles;
+drop table pci_revistas_anualidades;
+drop table pci_revistas;
+drop table pci_editoriales;
+drop table pci_libros;
+
+create table pci_revistas as
+  select * from uji_investigacion.pci_revistas@alma;
+
+create table pci_revistas_anualidades as
+  select * from uji_investigacion.pci_revistas_anualidades@alma;
+
+create table pci_revistas_cuartiles as
+  select * from uji_investigacion.pci_revistas_cuartiles@alma;
+
+create table pci_editoriales as
+  select * from uji_investigacion.pci_editoriales@alma;
+
+create table pci_libros as
+  select * from uji_investigacion.pci_libros@alma;
+
+drop table pci_producciones_autores;
+drop table pci_producciones_detalle;
+drop table pci_producciones_estados;
+drop table pci_producciones;
+
+create table pci_producciones as
+  select * from uji_investigacion.pci_producciones@alma;
+
+create table pci_producciones_autores as
+  select * from uji_investigacion.pci_producciones_autores@alma;
+
+create table pci_producciones_detalle as
+  select * from uji_investigacion.pci_producciones_detalle@alma;
+
+create table pci_producciones_estados as
+  select * from uji_investigacion.pci_producciones_estados@alma;
+
+drop table pci_producciones_01;
+drop table pci_producciones_02;
+drop table pci_producciones_03;
+drop table pci_producciones_04;
+drop table pci_producciones_05;
+drop table pci_producciones_06;
+drop table pci_producciones_07;
+drop table pci_producciones_09;
+drop table pci_producciones_10;
+drop table pci_producciones_11;
+drop table pci_producciones_12;
+drop table pci_producciones_13;
+drop table pci_producciones_14;
+drop table pci_producciones_15;
+drop table pci_producciones_16;
+drop table pci_producciones_17;
+drop table pci_producciones_18;
+
 create table pci_producciones_01 as
   select * from uji_investigacion.pci_producciones_01@alma;
 
@@ -50,5 +106,9 @@ create table pci_producciones_18 as
   select * from uji_investigacion.pci_producciones_18@alma;
 
 
+drop table cvn_view_direccion_tesis_pci;
+
 create table cvn_view_direccion_tesis_pci as
   select * from cvn_view_direccion_tesis_pci@alma;
+
+
